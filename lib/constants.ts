bfe6f557d11f939ebba76b6285e7a82603340b6b
Rob Ennals <rob@ennals.org>
2026-06-07 21:05:12 -0700

    feat: add feedback constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,1 +1,7 @@
 export const SITE_URL = 'https://acxreviews.robennals.org';
+
+// Feedback feature: only 2026 reviews have author contact data, so the
+// feedback form is offered for that contest year only.
+export const FEEDBACK_CONTEST_YEAR = 2026;
+export const FEEDBACK_NAME_MAX = 100;
+export const FEEDBACK_MESSAGE_MAX = 5000;
